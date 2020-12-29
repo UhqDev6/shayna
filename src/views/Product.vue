f3383c8b4b6d1ce6ef9103919583cff24b44f518
Muhammad Dhiya Ulhaq <uhqdev@gmail.com>
2020-12-29 10:38:47 +0800

    penambahan fungsi detail product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -49,20 +49,14 @@
                         <div class="col-lg-6">
                             <div class="product-details text-left">
                                 <div class="pd-title">
-                                    <span>oranges</span>
-                                    <h3>Pure Pineapple</h3>
+                                    <span>{{ productDetails.type }}</span>
+                                    <h3>{{productDetails.name}}</h3>
                                 </div>
                                 <div class="pd-desc text-justify">
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                        {{ productDetails.description }}
                                     </p>
-                                    <p>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                                    </p>
-                                    <h4>$495.00</h4>
+                                    <h4>{{ productDetails.price }}</h4>
                                 </div>
                                 <div class="quantity">
                                     <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
@@ -90,7 +84,7 @@ import RelatedProdukShayna from '../components/RelatedProdukShayna';
 import FooterShayna from '../components/FooterShayna';
 
 import carousel from 'vue-owl-carousel';
-
+import axios from 'axios';
 export default {
   name: 'product',
   components: {
@@ -101,23 +95,39 @@ export default {
   },
   data (){
       return {
-      gambar : "img/mickey1.jpg",
+      gambar : "",
       thumbs : [
           "img/mickey1.jpg",
           "img/mickey2.jpg",
           "img/mickey3.jpg",
           "img/mickey4.jpg"
       ],
-      idPoduct: this.$route.params.id
+      productDetails : []
       }
 
   },
   methods: {
       changeImage (urlImage){
           this.gambar = urlImage;
-          console.log(this.idPoduct);
+        //   console.log(this.idPoduct);
+      },
+      setDataPicture(data){
+          //replace object productDetail dengan data dari API 
+          this.productDetails = data;
+          // replace value gambar default dengan data dari API (Galleries)
+          this.gambar = data.galleries[0].photo;
       }
-  }
+  },
+  mounted() {
+      axios
+      .get("http://shayna-backend.belajarkoding.com/api/products", {
+          params : {
+              id : this.$route.params.id
+          }
+      })
+      .then(res => (this.setDataPicture(res.data.data)))
+      .catch(err => console.log(err));
+  },
 };
 </script>
 
