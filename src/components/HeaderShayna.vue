3ce885174438026ee4e8b49ff4648e8258bd8497
Muhammad Dhiya Ulhaq <uhqdev@gmail.com>
2020-12-30 16:57:14 +0800

    penambahan cart part 8

--- src/components/HeaderShayna.vue
+++ src/components/HeaderShayna.vue
@@ -61,7 +61,7 @@
                                     </div>
                                     <div class="select-total">
                                         <span>total:</span>
-                                        <h5>$120.00</h5>
+                                        <h5>${{ totalHarga }}. 00</h5>
                                     </div>
                                     <div class="select-button">
                                         <a href="#" class="primary-btn view-card"><router-link to="/cart" style="color:#ffffff">VIEW CARD</router-link></a>
@@ -100,6 +100,13 @@ export default {
           } catch (error) {
               localStorage.removeItem('keranjanguser');
           }
+      }
+  },
+  computed : {
+      totalHarga () {
+          return this.keranjangUser.reduce(function (items, data) {
+              return items + data.price;
+          }, 0);
       }
   }
 }
